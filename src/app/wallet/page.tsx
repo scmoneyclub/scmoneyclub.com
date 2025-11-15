b4b4a1ce965e3e7902707fb2083aeabe8f93a030
Justin Estrada <6895474+justinestrada@users.noreply.github.com>
2025-11-15 16:37:55 -0700

    Phantom Wallet - Assets

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -8,7 +8,7 @@ export default function WalletPage() {
     <div className="flex flex-col min-h-screen items-center justify-center bg-black text-gray-300">
       <Header />
       <main className="w-full">
-        <PhantomWalletAssets solana_address="5GhkB9Ke8V2GeGKhsr2Yy2bf53mMBHMnme3r18ESyhpa"/>
+        <PhantomWalletAssets/>
       </main>
       <Footer />
     </div>
